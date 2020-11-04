9bf5107ad63bd1dfe277c0ecc05572c6db263ac9
VishnuChakravarti13 <73475988+VishnuChakravarti13@users.noreply.github.com>
2020-11-04 16:51:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,28 +1,51 @@
 var rect1, rect2;
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(1000,500);
  rect1 = createSprite(500, 100, 50, 50);
  rect1.shapeColor = "blue";
- rect2 = createSprite(400, 200, 60, 60);
+ rect1.velocityY = 5;
+ rect2 = createSprite(500, 800, 60, 60);
  rect2.shapeColor = "red";
+ rect2.velocityY = -5;
 }
 
 function draw() {
   background(0);  
-  rect1.x = mouseX;
-  rect1.y = mouseY;
-  if(rect1.x - rect2.x < rect1.width/2 + rect2.width/2 && 
-    rect2.x - rect1.x < rect1.width/2 + rect2.width/2 &&
-    rect1.y - rect2.y < rect1.height/2 + rect2.height/2 && 
-    rect2.y - rect1.y < rect1.height/2 + rect2.height/2  ){
-    rect1.shapeColor = "white";
-    rect2.shapeColor = "white";
-  }
-  else{
-    rect1.shapeColor = "blue";
-    rect2.shapeColor = "red";
-  };
+  //rect1.x = mouseX;
+  //rect1.y = mouseY;
+  bounceoff(rect2,rect1);
   drawSprites();
-
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
